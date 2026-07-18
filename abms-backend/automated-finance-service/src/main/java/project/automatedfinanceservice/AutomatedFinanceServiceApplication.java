4249a453eef694b72b22c1f467929ba1f3f15877
dungnthe <167021991+DungTien04@users.noreply.github.com>
2026-07-18 15:04:33 +0700

    create service

---
+++ abms-backend/automated-finance-service/src/main/java/project/automatedfinanceservice/AutomatedFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package project.automatedfinanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedFinanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedFinanceServiceApplication.class, args);
+    }
+
+}
---
+++ abms-backend/automated-finance-service/src/test/java/project/automatedfinanceservice/AutomatedFinanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.automatedfinanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedFinanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
