e8928e7fbec08c1f198e953fd2069d929ad8b0de
vinhrut <nguyenxuanvinh2004tdt@gmail.com>
2026-07-19 22:19:00 +0700

    merge huy

--- abms-backend/notification-service/src/main/java/com/csms/notification/NotificationServiceApplication.java
+++ abms-backend/notification-service/src/main/java/com/csms/notification/NotificationServiceApplication.java
@@ -1,5 +1,7 @@
 package com.csms.notification;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
